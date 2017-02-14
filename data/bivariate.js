377714f2e3c8348beac2827c6b6d7c90aa54d93e
Julien Barnier <julien@nozav.org>
2017-02-14 18:02:19 +0100

    Reorganize datasets

---
+++ data/bivariate.js
@@ -0,0 +1,16 @@
+function datasets(id) {
+
+    var str;
+
+    switch(id) {
+	
+    case "life_expectancy_2014":
+	// Life expectancy by country in 2014. Source: World bank.
+	str = "";
+	break;
+	
+    }
+    return str;
+}
+
+

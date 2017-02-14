8fe2a861fcd1d6fdbf757b00983e04c99cb26858
Julien Barnier <julien@nozav.org>
2017-02-14 18:02:34 +0100

    Update translations

--- js/l10n/localizations.js
+++ js/l10n/localizations.js
@@ -2,11 +2,17 @@ String.toLocaleString({
     "en": {
 	":median:": "Median",
 	":mean:": "Mean",
-	":sd:": "Standard deviation"
+	":sd:": "Standard deviation",
+	":sd_short:": "sd",
+	":correlation:": "Correlation",
+	":covariance:": "Covariance"
     },
     "fr": {
 	":median:": "Médiane",
 	":mean:": "Moyenne",
-	":sd:": "Écart-type"
+	":sd:": "Écart-type",
+	":sd_short:": "sd",
+	":correlation:": "Corrélation",
+	":covariance:": "Covariance"
     }
 });
